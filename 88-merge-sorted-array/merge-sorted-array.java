a2d5210b00355fa17bd43702237b5d2f1f122c58
Inder Singh <67420435+SinghInder101@users.noreply.github.com>
2024-09-26 21:24:16 -0400

    Time: 0 ms (100.00%) | Memory: 42.1 MB (63.10%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,26 +1,48 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1;  // Pointer for nums1
-        int p2 = n - 1;  // Pointer for nums2
-        int p = m + n - 1;  // Pointer for the final position in nums1
-        
-        // Merge nums2 into nums1 starting from the end
-        while (p1 >= 0 && p2 >= 0) {
-            if (nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1];
-                p1--;
-            } else {
-                nums1[p] = nums2[p2];
-                p2--;
+
+ArrayList<Integer> list = new ArrayList<Integer>();
+int oneIndex = 0;
+int secondIndex =0;
+
+
+        for(int i = 0 ; i < m+n ;i++){
+            if(oneIndex<m && secondIndex<n){
+                if(nums1[oneIndex] <= nums2[secondIndex]){
+                    list.add(nums1[oneIndex]);
+                    oneIndex++;
+                    continue;
+                }
+                else if(nums1[oneIndex] > nums2[secondIndex]){
+                    list.add(nums2[secondIndex]);
+                    secondIndex++;
+                    continue;
+
+                }   
+
             }
-            p--;
+            if(oneIndex == m && secondIndex < n){
+                 list.add(nums2[secondIndex]);
+                    secondIndex++;
+                    continue;
+
+
+            }
+            else if(secondIndex == n && oneIndex < m){
+                list.add(nums1[oneIndex]);
+                    oneIndex++;
+                    continue;
+
+            }
+            
+
+        }
+
+         for (int i = 0; i < m + n; i++) {
+            nums1[i] = list.get(i);
         }
         
-        // If there are remaining elements in nums2, copy them over to nums1
-        while (p2 >= 0) {
-            nums1[p] = nums2[p2];
-            p--;
-            p2--;
-        }
-    }
+        
+
+}
 }
