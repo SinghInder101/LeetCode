c4e854b7eb8ec99f10d7f05a674e9cd6ffbba508
Inder Singh <67420435+SinghInder101@users.noreply.github.com>
2024-09-30 18:18:18 -0400

    Time: 7 ms (49.95%) | Memory: 44.6 MB (22.83%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,31 +1,34 @@
 class Solution {
-    private Map<String, Boolean> memo = new HashMap<>();
+     private Map<String, Boolean> memo = new HashMap<>();
 
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordSet = new HashSet<>(wordDict);
-        return solve(s, wordSet);
+         Set<String> wordSet = new HashSet<>(wordDict);
+
+         return solve(s,wordSet);
+
+        
+        
     }
 
-    public boolean solve(String s, Set<String> wordDict) {
-        if (s.length() == 0) {
+    public boolean solve(String s, Set<String> wordDict){
+  
+        if(s.length() == 0){
             return true;
         }
-
-        if (memo.containsKey(s)) {
+        
+        if(memo.containsKey(s)){
             return memo.get(s);
         }
 
-        for (int i = 1; i <= s.length(); i++) {
-            String prefix = s.substring(0, i);
-            if (wordDict.contains(prefix)) {
-                System.out.println("Prefix found: " + prefix);  // Debugging: print found prefixes
-            }
-            if (wordDict.contains(prefix) && solve(s.substring(i), wordDict)) {
-                memo.put(s, true);
+        for(int i =1; i<=s.length() ;i++){
+            if(wordDict.contains(s.substring(0,i)) && solve(s.substring(i),wordDict)){
+                memo.put(s,true);
                 return true;
+
             }
         }
-        memo.put(s, false);
+        memo.put(s,false);
         return false;
+
     }
 }
