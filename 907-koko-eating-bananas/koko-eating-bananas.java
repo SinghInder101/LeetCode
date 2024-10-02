fcca23f4bf65314737b17193e17a6d3894c0f835
Inder Singh <67420435+SinghInder101@users.noreply.github.com>
2024-10-01 21:33:39 -0400

    Time: 25 ms (48.71%) | Memory: 44.9 MB (63.65%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+          // Initialize max and min values to the first element in the array
+        int maxValue = piles[0];
+
+        // Loop through the array to find the maximum, minimum values, and calculate the sum
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > maxValue) {
+                maxValue = piles[i];
+            }
+           
+        }
+
+        if(piles.length == h ){
+            return maxValue;
+        }
+      int low = 1, high = maxValue;
+
+      while(low<=high){
+        int mid = (high+low)/2;
+
+        int totalHours = helper(piles,mid);
+        if(totalHours <= h){
+            high  = mid-1;
+        }else{
+            low=mid+1;
+
+        }
+
+
+      }
+      return low;
+       
+        
+        
+    }
+    public int helper(int[] piles, int currSpeed){
+        int totalHours = 0;
+        for(int i = 0; i<piles.length ; i++){
+
+            totalHours += Math.ceil((double) piles[i]/(double) currSpeed);
+
+
+
+
+        }
+        return totalHours;
+    }
+}
