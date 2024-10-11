aa0f8f6ae1177e56f3a8f4a2629148e425138e4b
Inder Singh <67420435+SinghInder101@users.noreply.github.com>
2024-10-10 20:38:10 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (85.70%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,29 +1,42 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        if(nums.length==1) return 0;
+        int left = 1;
+        int right = nums.length-2;
+        if(nums.length == 1){
+            return 0;
+        }
+       if(nums[0]>nums[1]){
+        return 0;
+       }
+       if(nums[nums.length-1]>nums[nums.length-2]){
+        return nums.length-1;
+       }
 
-        return binarySearch(0,nums.length-1,nums);
+
+        return bs(nums,left,right);
+
+        
     }
+    public int bs(int[] nums, int left,int right){
+        if(left<=right){
+            int mid = (left+right)/2;
 
-    int binarySearch(int l , int r ,int[] nums){
-        int mid = l+(r-l)/2;
-    
-        if(mid ==0 ){
-            return nums[mid+1]<nums[mid]?mid:mid+1;
-        }
+            if(nums[mid-1] < nums[mid] && nums[mid+1] < nums[mid] ){
+                return mid;
+            }
+            else{
+                if(nums[mid-1] < nums[mid] && nums[mid+1] > nums[mid]){
+                    return bs(nums,mid+1,right);
+                }
+                if(nums[mid-1]>nums[mid] && nums[mid+1] < nums[mid]){
+                    return bs(nums,left,mid-1);
+                }
+                if(nums[mid-1] > nums[mid] && nums[mid+1] > nums[mid]){
+                    return bs(nums,mid+1,right);
+            }
+            }
 
-        if(mid ==nums.length-1 && nums[mid-1]<nums[mid]){
-            return mid;
         }
-
-        if(nums[mid-1] <nums[mid] && nums[mid+1]<nums[mid]){
-            return mid;
-        }
-
-        if(nums[mid-1]>nums[mid]) {
-            return binarySearch(l,mid,nums);
-        } else {
-             return binarySearch(mid+1,r,nums);
-        }
+        return -1;
     }
 }
