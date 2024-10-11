196eabff0971e01f899a1d4ed12bb7ea272aa917
Inder Singh <67420435+SinghInder101@users.noreply.github.com>
2024-10-10 22:53:44 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (93.85%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,25 +1,48 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 1;
-        int right = nums.length-2;
-        if(nums.length == 1){
-            return 0;
-        }
-       if(nums[0]>nums[1]){
-        return 0;
-       }
-       if(nums[nums.length-1]>nums[nums.length-2]){
-        return nums.length-1;
-       }
+        int left = 0;
+        int right = nums.length-1;
+    //     if(nums.length == 1){
+    //         return 0;
+    //     }
+    //    if(nums[0]>nums[1]){
+    //     return 0;
+    //    }
+    //    if(nums[nums.length-1]>nums[nums.length-2]){
+    //     return nums.length-1;
+    //    }
 
 
-        return bs(nums,left,right);
+    //     
+    return bs(nums,left,right);
 
         
     }
     public int bs(int[] nums, int left,int right){
         if(left<=right){
             int mid = (left+right)/2;
+
+            if(mid == 0){
+                if(nums.length == 1){
+                    return 0;
+                }
+                else if(nums[mid] > nums[mid+1]){
+                    return 0;
+
+                }
+                else if(nums[mid+1] > nums[mid]){
+                    return mid+1;
+                }
+
+            }
+            if(mid == nums.length-1){
+                if(nums[mid] > nums[mid-1]){
+                    return mid;
+                }
+                else{
+                    return mid-1;
+                }
+            }
 
             if(nums[mid-1] < nums[mid] && nums[mid+1] < nums[mid] ){
                 return mid;
