f1bc44de0f44032913efc602b2d532dabfccdcd0
Inder Singh <67420435+SinghInder101@users.noreply.github.com>
2024-10-10 22:57:45 -0400

    Time: 0 ms (100.00%) | Memory: 42.1 MB (72.94%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,19 +1,22 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 0;
-        int right = nums.length-1;
-    //     if(nums.length == 1){
-    //         return 0;
-    //     }
-    //    if(nums[0]>nums[1]){
-    //     return 0;
-    //    }
-    //    if(nums[nums.length-1]>nums[nums.length-2]){
-    //     return nums.length-1;
-    //    }
+        int left = 1;
+        int right = nums.length-2;
+        if(nums.length == 1){ //Agar ek length ka hai toh sidha return
+            return 0;
+        }
+       if(nums[0]>nums[1]){
+         //If the first element is greater than the second, it means it is a  maxima return it
+        return 0;
+       }
+       if(nums[nums.length-1]>nums[nums.length-2]){
+        //If the last element is greater thant the second last, it means it is the maxima return it.
+        return nums.length-1;
+       }
 
 
-    //     
+    //     If first and last both are not the answer, the answer must lie between them therefore
+     // left = 1  and right = second_Last(nums.length-2)(line 4), we apply the bs in them
     return bs(nums,left,right);
 
         
@@ -22,27 +25,27 @@ class Solution {
         if(left<=right){
             int mid = (left+right)/2;
 
-            if(mid == 0){
-                if(nums.length == 1){
-                    return 0;
-                }
-                else if(nums[mid] > nums[mid+1]){
-                    return 0;
+            // if(mid == 0){
+            //     if(nums.length == 1){
+            //         return 0;
+            //     }
+            //     else if(nums[mid] > nums[mid+1]){
+            //         return 0;
 
-                }
-                else if(nums[mid+1] > nums[mid]){
-                    return mid+1;
-                }
+            //     }
+            //     else if(nums[mid+1] > nums[mid]){
+            //         return mid+1;
+            //     }
 
-            }
-            if(mid == nums.length-1){
-                if(nums[mid] > nums[mid-1]){
-                    return mid;
-                }
-                else{
-                    return mid-1;
-                }
-            }
+            // }
+            // if(mid == nums.length-1){
+            //     if(nums[mid] > nums[mid-1]){
+            //         return mid;
+            //     }
+            //     else{
+            //         return mid-1;
+            //     }
+            // }
 
             if(nums[mid-1] < nums[mid] && nums[mid+1] < nums[mid] ){
                 return mid;
