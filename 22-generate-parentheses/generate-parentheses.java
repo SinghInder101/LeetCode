76eb32dfa1a632a0aeef08b37c3e2be45705b2bf
Inder Singh <67420435+SinghInder101@users.noreply.github.com>
2024-10-12 18:34:36 -0400

    Time: 17 ms (5.44%) | Memory: 59 MB (6.04%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,40 +1,50 @@
-class Solution {
-    int ans = 0;
-    List<String> fin = new ArrayList<>();
+import java.util.*;
 
-    public List<String> generateParenthesis(int n) {
+public class Solution {
+    private Map<String, List<String>> memo = new HashMap<>(); // Memoization cache
+    private List<String> fin = new ArrayList<>(); // Final results list
+    private int ans = 0; // Counter for valid combinations
 
-        helper(n, 0, 0,"");
-        System.out.println(ans);
-        return fin;
-
-    }
-
-    public void helper(int n, int open, int close,String s) {
+    public void helper(int n, int open, int close, String s) {
+        // Base case: If open and close are both equal to n
         if (open == n && close == n) {
             ans++;
             fin.add(s);
             return;
         }
 
-        if (open == 0) {
+        // Create a unique key for the current state
+        String key = open + "," + close + "," + s;
 
-            helper(n, open + 1, close,s+"(");
-
-        } else {
-
-            if (open == n && close < n) {
-                helper(n, open, close + 1,s+")");
-
-            } else if (open < n && close < n) {
-                helper(n, open + 1, close,s+"(");
-                if(close<open){
-                helper(n, open, close + 1,s+")");
-                }
-
-            }
-
+        // If the result for this state is already computed, return
+        if (memo.containsKey(key)) {
+            fin.addAll(memo.get(key));
+            return;
         }
 
+        List<String> currentResult = new ArrayList<>();
+
+        if (open == 0) {
+            helper(n, open + 1, close, s + "(");
+        } else {
+            if (open == n && close < n) {
+                helper(n, open, close + 1, s + ")");
+            } else if (open < n && close < n) {
+                helper(n, open + 1, close, s + "(");
+                if (close < open) {
+                    helper(n, open, close + 1, s + ")");
+                }
+            }
+        }
+
+        // Store the result for this state in the memoization map
+        memo.put(key, new ArrayList<>(fin));
     }
+
+    public List<String> generateParenthesis(int n) {
+        helper(n, 0, 0, "");
+        return fin;
+    }
+
+  
 }
